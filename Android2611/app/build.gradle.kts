659d12c26451a23e03a95cbea7d9eb6a2b0784ae
unknown <huynguyenbinh@gmail.com>
2024-12-02 23:40:44 +0700

    first commit

---
+++ Android2611/app/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+  alias(libs.plugins.android.application)
+  alias(libs.plugins.kotlin.android)
+}
+
+android {
+  namespace = "vn.edu.hust.studentman"
+  compileSdk = 35
+
+  defaultConfig {
+    applicationId = "vn.edu.hust.studentman"
+    minSdk = 24
+    targetSdk = 34
+    versionCode = 1
+    versionName = "1.0"
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+  }
+  kotlinOptions {
+    jvmTarget = "11"
+  }
+}
+
+dependencies {
+
+  implementation(libs.androidx.core.ktx)
+  implementation(libs.androidx.appcompat)
+  implementation(libs.material)
+  implementation(libs.androidx.activity)
+  implementation(libs.androidx.constraintlayout)
+  testImplementation(libs.junit)
+  androidTestImplementation(libs.androidx.junit)
+  androidTestImplementation(libs.androidx.espresso.core)
+  implementation(libs.androidx.navigation.ui.ktx)
+  implementation(libs.androidx.navigation.fragment.ktx)
+  val fragment_version = "1.8.5"
+  // Kotlin
+  implementation("androidx.fragment:fragment-ktx:$fragment_version")
+
+}
